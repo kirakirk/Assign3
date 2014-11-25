e821bed889a3717a86a3e50fe692c76af1422c62
kirakirk <kirakirk@uvic.ca>
2014-11-24 20:45:06 -0800

    finished part 1

--- 3_2.c
+++ 3_2.c
@@ -12,47 +12,78 @@ Submitted by: Kira Kirk - V00705087
 
 void readSuperBlockInfo(FILE *diskImage)
 {
-	unsigned int blockSize, fileSystemSize, FATstart, FATblocks, rootStart, rootBlocks;
+	unsigned int blockSize, fileSystemSize, FATstart, FATblocks, rootStart, rootBlocks, temp;
+	int freeBlocks = 0;
+	int resBlocks = 0;
+	int alloBlocks = 0;
 	int i;
 	//unsigned ints are 4 bytes
- 
-	//uint32_t
 
 	fseek(diskImage, 8, SEEK_SET);
 
-	printf("Super block information:\n");
+	printf("\nSuper block information:\n");
     
     fread(&blockSize, 2, 1, diskImage);
     blockSize = ntohs(blockSize);
-	printf("Block size: %X\n", blockSize);
+	printf("Block size: %d\n", blockSize);
 
 	fread(&fileSystemSize, 4, 1, diskImage);
     fileSystemSize = ntohl(fileSystemSize);
-	printf("Block count: %X\n", fileSystemSize);
+	printf("Block count: %d\n", fileSystemSize);
 
 	fread(&FATstart, 1, 4, diskImage);	//might need to be 4, 1 not 1, 4
     FATstart = ntohl(FATstart);
-	printf("FAT starts: %X\n", FATstart);
+	printf("FAT starts: %d\n", FATstart);
 
 	fread(&FATblocks, 4, 1, diskImage);
 	FATblocks = ntohl(FATblocks);
-	printf("FAT blocks: %X\n", FATblocks);
+	printf("FAT blocks: %d\n", FATblocks);
 
 	fread(&rootStart, 4, 1, diskImage);
 	rootStart = ntohl(rootStart);
-	printf("Root directory start: %X\n", rootStart);
+	printf("Root directory start: %d\n", rootStart);
 
 	fread(&rootBlocks, 4, 1, diskImage);
 	rootBlocks = ntohl(rootBlocks);
-	printf("Root directory blocks: %X\n", rootBlocks);
+	printf("Root directory blocks: %d\n\n", rootBlocks);
 
-	i = FATstart*512;
+	//move file pointer to the start of the FAT
+	fseek(diskImage, (FATstart*blockSize), SEEK_SET);
 
-	fseek(diskImage, 512, SEEK_SET);
-
-	printf("FAT information: i = %d\n", i);
-
+	//read through the FAT to get the information
+	//there might be a better way to do this
+	for (i = 0; i < fileSystemSize; i++)
+	{
+		fread(&temp, 4, 1, diskImage);
+    	temp = ntohl(temp);
+    	if (temp == 0x00000000)	//this block is available
+    	{
+    		freeBlocks++;
+    	}
+    	else if(temp == 0x00000001)	//this block is reserved
+    	{
+    		resBlocks++;
+    	}
+    	else if (temp >= 0x00000002 && temp <= 0xFFFFFF00)	//this block is allocated as part of a file
+    	{
+    		alloBlocks++;
+    	}
+    	else if (temp == 0xFFFFFFFF)	//this is the last block in a file and therefore allocated to the file
+    	{
+    		alloBlocks++;
+    	}
+    	else
+    	{
+    		printf("Error Reading FAT: FAT entry is invalid\n");
+    	}
+	}
+	
+	printf("FAT information: \n");	
+	printf("Free Blocks: %d\n", freeBlocks);
+	printf("Reserved Blocks: %d\n", resBlocks);
+	printf("Allocated Blocks: %d\n", alloBlocks);
 }
+
 
 int main (int argc, char *argv[])
 {
@@ -68,23 +99,26 @@ int main (int argc, char *argv[])
 
 	readSuperBlockInfo(diskImage);
 
+	//readFATInfo(diskImage);
+
 	fclose(diskImage);
 }
 
 
 /*
-Super block information:
-Block size: 512
-Block count: 5120
-FAT starts: 1
-FAT blocks: 40
-Root directory start: 41
+block 0 = superblock
+block 2 = FAT for 32 blocks, maybe not consecutive
+block 35 = root directory for 8 blocks, maybe not consecutive
+
+
+Block size: 200
+file system size in blocks = Block count: 1900
+FAT starts: 2
+FAT blocks: 32
+Root directory start: 35
 Root directory blocks: 8
 
-FAT information:
-Free Blocks: 5071
-Reserved Blocks: 41
-Allocated Blocks: 8
+512 byte blocks
 
 Description 						Size 	 Default Value
 File system identifier  			8 bytes  CSC360FS 
